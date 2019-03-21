6817a6f00362298f5ba31d3f53c8972252b6b682
hugo <hugo@nuaa.edu.cn>
2019-03-21 22:38:28 +0800

    add merge sort

---
+++ interview/Data Stucture/Sort/merge_sort.py
@@ -0,0 +1,34 @@
+def merge_sort(nums):
+    if nums and len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+
+
+if __name__ == '__main__':
+    nums = [12, 11, 13, 5, 6, 7]
+    merge_sort(nums)
+    print(nums)
