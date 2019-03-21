e95284971ad3f888b61658349e9296529c05e22d
hugo <hugo@nuaa.edu.cn>
2019-03-21 11:58:45 +0800

    add binary tree pre-order traversal

---
+++ 144/python/PreorderTraversal.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def preorderTraversal_recursive(self, root: TreeNode) -> List[int]:
+        res = []
+
+        def f(cur):
+            if cur:
+                res.append(cur.val)
+                f(cur.left)
+                f(cur.right)
+
+        f(root)
+        return res
+
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        stack = []
+        cur = root
+        while cur or stack:
+            while cur:
+                res.append(cur.val)
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            cur = cur.right
+        return res
